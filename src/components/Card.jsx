6aaba71fc162f95abbc5516cacd9635f6f77d91c
Shubham73-dev <79575532+Shubham73-dev@users.noreply.github.com>
2023-05-09 13:41:59 +0530

    Add files via upload

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const Card = ({ data }) => {
+    const [flag, setFlag] = useState(false);
+    const operation = (event) => {
+        setFlag(!flag)
+    }
     return (
         <div className='card'>
-            <h4>{data.name}</h4>
+            <div className="chevron-container">
+                <h4>{data.name}</h4>
+                <span onClick={operation}>{flag ? <ExpandMoreIcon />:<ChevronRightIcon />}</span>
+            </div>
             <p>{data.study_short_description}</p>
-            {data.child_subject.map((value, index) => {
-                return (
-                    <div className="slugsContainer" key={index}>
-                        <div className="slug_left">
-                            <h5>{value.name}</h5>
-                            <span>{value.no_of_doc} documents</span>
+            <div className={`slugChilds ${flag ? 'showSlugChilds' : 'hideSlugChilds'}`}>
+                {data.child_subject.map((value, index) => {
+                    return (
+                        <div className="slugsContainer" key={index}>
+                            <div className="slug_left">
+                                <h5>{value.name}</h5>
+                                <span>{value.no_of_doc} documents</span>
+                            </div>
+                            <div className="slug_right"><ArrowRightAltIcon /></div>
                         </div>
-                        <div className="slug_right"><ArrowRightAltIcon /></div>
-                    </div>
-                )
-            })}
+                    )
+                })}
+            </div>
+
         </div>
     )
 }
